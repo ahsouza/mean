7f06a229f9df9b19cc12d3b96bb451f1fc4a2e77
Aníbal Henrique <annibalhsouza@gmail.com>
2021-02-13 05:17:01 -0300

    #adicionando novos components e serviços

---
+++ src/app/components/veiculo-details/veiculo-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-veiculo-details',
+  templateUrl: './veiculo-details.component.html',
+  styleUrls: ['./veiculo-details.component.css']
+})
+export class VeiculoDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
