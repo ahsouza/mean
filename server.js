e44658f608933ce1e07c0c9eeb3a60877902b47e
Aníbal Henrique <annibalhsouza@gmail.com>
2021-02-13 04:02:42 -0300

    #implementando servidor

---
+++ server.js
@@ -0,0 +1,25 @@
+const
+ express = require("express"),
+ bodyParser = require("body-parser"),
+ cors = require("cors")
+
+const app = express()
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+}
+
+app.use(cors(corsOptions))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Seja bem vindo ao MEAN APP." })
+})
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`)
+})
