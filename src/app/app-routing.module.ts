f763c16f9e1ff8b21c4d032457b7c94315fdd83d
Aníbal Henrique <annibalhsouza@gmail.com>
2021-02-15 18:37:55 -0300

    preparando hosting com Firebase

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { VeiculoListComponent } from './components/veiculo-list/veiculo-list.component';
+import { VeiculoDetailsComponent } from './components/veiculo-details/veiculo-details.component';
+import { AddVeiculoComponent } from './components/add-veiculo/add-veiculo.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'add', pathMatch: 'full' },
+  { path: 'veiculos', component: VeiculoListComponent },
+  { path: 'veiculos/:id', component: VeiculoDetailsComponent },
+  { path: 'add', component: AddVeiculoComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
