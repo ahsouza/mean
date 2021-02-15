584b89a5271e2824ccbaa7cdffb533cad16e0f12
Aníbal Henrique <annibalhsouza@gmail.com>
2021-02-15 14:20:28 -0300

    #backend with success

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const
+ configDB = require("../config/database/db.js"),
+ Sequelize = require("sequelize")
+
+const sequelize = new Sequelize(configDB.DB, configDB.USER, configDB.PASSWORD, {
+  host: configDB.HOST,
+  dialect: configDB.dialect,
+  operatorsAliases: false,
+  pool: {
+    max: configDB.pool.max,
+    min: configDB.pool.min,
+    acquire: configDB.pool.acquire,
+    idle: configDB.pool.idle
+  }
+})
+
+const db = {}
+
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+
+db.veiculos = require("./veiculo.model.js")(sequelize, Sequelize)
+
+module.exports = db;
