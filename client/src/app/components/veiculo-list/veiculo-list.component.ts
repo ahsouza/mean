0590c88dfb45c43e610e0e8be39a7ae3404e6e72
Aníbal Henrique <annibalhsouza@gmail.com>
2021-02-23 00:01:39 -0300

    Adicionando spinner UI e método pré-load
    Adicionando spinner UI Material Design e métodos para pré-carregamento em conteúdos do componente de listagem para veículos

--- client/src/app/components/veiculo-list/veiculo-list.component.ts
+++ client/src/app/components/veiculo-list/veiculo-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { VeiculoService } from 'src/app/services/veiculo.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Veiculo } from 'src/app/models/veiculo.model';
+import { Component, OnInit } from '@angular/core'
+import { VeiculoService } from 'src/app/services/veiculo.service'
+import { ActivatedRoute, Router } from '@angular/router'
+import { Veiculo } from 'src/app/models/veiculo.model'
+import { ProgressSpinnerMode } from '@angular/material/progress-spinner'
 
 @Component({
   selector: 'app-veiculo-list',
@@ -9,16 +10,22 @@ import { Veiculo } from 'src/app/models/veiculo.model';
   styleUrls: ['./veiculo-list.component.css']
 })
 export class VeiculoListComponent implements OnInit {
-  
-  veiculos?: Veiculo[];
-  currentVeiculo?: Veiculo;
-  currentIndex = -1;
-  placa = '';
-  chassi = 0;
-  renavam = 0;
-  modelo = 0;
-  marca = '';
-  ano = 0;
+  title = 'Veículo'
+  subtitle = 'Escolha algum veículo'
+  buttons = ['Excluír', 'Editar']
+  submitted = false
+  hidden=true
+  labels = ['Placa', 'Chassi', 'Renavam', 'Marca', 'Modelo', 'Ano']
+  spinnerMode: ProgressSpinnerMode = 'determinate'
+  veiculos?: Veiculo[]
+  currentVeiculo?: Veiculo
+  currentIndex = -1
+  placa = ''
+  chassi = 0
+  renavam = 0
+  modelo = 0
+  marca = ''
+  ano = 0
 
   constructor(
     private veiculoService: VeiculoService,
@@ -27,30 +34,38 @@ export class VeiculoListComponent implements OnInit {
   { }
 
   ngOnInit(): void {
-  	this.retrieveVeiculos();
+  	this.retrieveVeiculos()
   }
 
   retrieveVeiculos(): void {
+    this.spinnerMode = 'indeterminate'
     this.veiculoService.getAll()
       .subscribe(
         data => {
-          this.veiculos = data;
-          console.log(data);
+          setTimeout(()=> {
+            this.spinnerMode = 'determinate'
+            this.veiculos = data
+          }, 600)
+          setTimeout(()=> {
+            this.hidden = false
+          }, 700)
+
+          console.log(data)
         },
         error => {
-          console.log(error);
+          console.log(error)
         });
   }
 
   refreshList(): void {
-    this.retrieveVeiculos();
-    this.currentVeiculo = undefined;
-    this.currentIndex = -1;
+    this.retrieveVeiculos()
+    this.currentVeiculo = undefined
+    this.currentIndex = -1
   }
 
   setActiveVeiculo(veiculo: Veiculo, index: number): void {
-    this.currentVeiculo = veiculo;
-    this.currentIndex = index;
+    this.currentVeiculo = veiculo
+    this.currentIndex = index
   }
 
   removeAllVeiculos(): void {
@@ -58,39 +73,38 @@ export class VeiculoListComponent implements OnInit {
       .subscribe(
         res => {
           console.log(res);
-          this.router.navigate(['/veiculos']);
-          this.retrieveVeiculos();
+          this.router.navigate(['/veiculos'])
+          this.retrieveVeiculos()
         },
         error => {
-          console.log(error);
-        });
+          console.log(error)
+        })
   }
 
   searchChassi(): void {
-    this.currentVeiculo = undefined;
-    this.currentIndex = -1;
+    this.currentVeiculo = undefined
+    this.currentIndex = -1
 
     this.veiculoService.findByChassi(this.chassi)
       .subscribe(
         data => {
-          this.veiculos = data;
-          console.log(data);
+          this.veiculos = data
+          console.log(data)
         },
         error => {
-          console.log(error);
-        });
+          console.log(error)
+        })
   }
 
   deleteVeiculo(id: number): void {
     this.veiculoService.delete(id)
       .subscribe(
         res => {
-          console.log(res);
-          window.location.reload();
+          console.log(res)
+          window.location.reload()
         },
         error => {
-          console.log(error);
-        });
+          console.log(error)
+        })
   }
-
 }
