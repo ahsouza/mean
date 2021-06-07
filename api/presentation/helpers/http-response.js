f1f88f49e56c60d93881f0c7fbb6e63f4366ad35
ahsouza <annibalhsouza@gmail.com>
2021-06-07 11:02:08 -0300

    refactor: refactoring http-reponse

---
+++ api/presentation/helpers/http-response.js
@@ -0,0 +1,31 @@
+const { UnauthorizedError, ServerError } = require('../errors')
+
+module.exports = class HttpResponse {
+  static badRequest (error) {
+    return {
+      statusCode: 400,
+      body: { error: error.message }
+    }
+  }
+
+  static serverError () {
+    return {
+      statusCode: 500,
+      body: { error: new ServerError().message }
+    }
+  }
+
+  static unauthorizedError () {
+    return {
+      statusCode: 401,
+      body: { error: new UnauthorizedError().message }
+    }
+  }
+
+  static success (data) {
+    return {
+      statusCode: 200,
+      body: data
+    }
+  }
+}
